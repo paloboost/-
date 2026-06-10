617308b51dc09cea3125797ccb0f83437c59d71a
Palo <jjy070806@gmail.com>
2026-06-11 00:35:28 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+gsap.from(".hero h1",{
+y:100,
+opacity:0,
+duration:1
+});
+
+gsap.from(".hero h2",{
+y:100,
+opacity:0,
+duration:1,
+delay:.3
+});
+
+gsap.from(".buttons",{
+y:100,
+opacity:0,
+duration:1,
+delay:.6
+});
